cb06b7a274eac85b0b635df2944b7995230efcb0
Jan Muthreich <jan@muthreich.eu>
2017-10-22 11:00:26 +0200

    Removed content accessor and make _content public

--- common/shared/src/main/scala/resources/Inventory.scala
+++ common/shared/src/main/scala/resources/Inventory.scala
@@ -2,23 +2,21 @@ package resources
 
 class Inventory(val resources: Seq[Resource], val maxSize: Int, init: Amount*) {
 
-    private val _content = new Array[Double](resources.size)
-    init.foreach(a => _content(a.resource.i) = if (a.amount > maxSize) maxSize else a.amount)
+    val content = new Array[Double](resources.size)
+    init.foreach(a => content(a.resource.i) = if (a.amount > maxSize) maxSize else a.amount)
 
-    def content: Seq[Amount] = resources.map(r => _content(r.i) ~ r)
-
-    def apply(resource: Resource): Double = _content(resource.i)
+    def apply(resource: Resource): Double = content(resource.i)
 
     def add(amounts: Amount*): Boolean = {
         var it = amounts.iterator
         while (it.hasNext) {
             val a = it.next()
-            if (_content(a.resource.i) + a.amount > maxSize) return false
+            if (content(a.resource.i) + a.amount > maxSize) return false
         }
         it = amounts.iterator
         while (it.hasNext) {
             val a = it.next()
-            _content(a.resource.i) = _content(a.resource.i) + a.amount
+            content(a.resource.i) = content(a.resource.i) + a.amount
         }
         true
     }
@@ -27,12 +25,12 @@ class Inventory(val resources: Seq[Resource], val maxSize: Int, init: Amount*) {
         var it = amounts.iterator
         while (it.hasNext) {
             val a = it.next()
-            if (_content(a.resource.i) - a.amount < 0) return false
+            if (content(a.resource.i) - a.amount < 0) return false
         }
         it = amounts.iterator
         while (it.hasNext) {
             val a = it.next()
-            _content(a.resource.i) = _content(a.resource.i) - a.amount
+            content(a.resource.i) = content(a.resource.i) - a.amount
         }
         true
     }
