c518e04416f3ef39951146bb4bbb43d61201086e
Jan Muthreich <jan@muthreich.eu>
2018-02-23 20:36:23 +0100

    Restart with mobx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,41 @@
 import "../less/index.less";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {Game} from "./game";
+import {action, observable} from "mobx";
+import {observer} from "mobx-react";
+
+class GameState {
+
+    @observable test: number = 1;
+
+}
+
+class Controller {
+    constructor(private state: GameState) {
+
+    }
+
+    @action
+    increase(state: GameState) {
+        state.test += 1;
+    }
+
+}
+
+@observer
+class Test extends React.Component<{ state: GameState, controller?: Controller }> {
+
+    controller: Controller = this.props.controller || new Controller(this.props.state);
+
+    render() {
+        const { state } = this.props;
+        return <div>
+            It Works {state.test}
+            <button onClick={() => this.controller.increase(state)}>Click Me!</button>
+        </div>
+    }
+}
 
 const div = document.createElement("div");
 document.body.appendChild(div);
-ReactDOM.render(<Game/>, div);
+ReactDOM.render(<Test state={new GameState()}/>, div);
